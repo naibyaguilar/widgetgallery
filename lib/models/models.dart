774ad2533a3a6bbcb11cc7ce8998c8986eb9d788
Fernando Ku Ku <fkuku@serempre.com>
2022-07-04 23:16:39 -0500

    Añadiendo la practica 3 sobre Sing Up

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,4 @@
-export 'package:widgetgallery/models/item_menus.dart';
+export 'package:widgetgallery/models/item_menu.dart';
+export 'package:widgetgallery/models/student.dart';
+export 'package:widgetgallery/models/gender.dart';
+export 'package:widgetgallery/models/country.dart';
